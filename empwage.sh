665f931a29515cbcba0bc524674217e95743a37c
shrutiP1 <shrutipatil13798@gmail.com>
2021-04-30 14:28:18 +0530

    Added feature to check employee present or abscent

--- empwage.sh
+++ empwage.sh
@@ -1,2 +1,10 @@
-echo "welcome
-"
+echo "welcome"
+#UC1
+isPresent=1;
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ];
+then
+     echo "employee is present";
+else
+     echo "employeee is absent";
+fi
